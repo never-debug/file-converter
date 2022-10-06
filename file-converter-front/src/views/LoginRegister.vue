6b95148fffbc41471c5f6cfb8b98393edf3792a3
runhuo1 <1984045322@qq.com>
2022-10-06 13:46:59 +0800

    v1.0.4

--- file-converter-front/src/views/LoginRegister.vue
+++ file-converter-front/src/views/LoginRegister.vue
@@ -1,38 +1,41 @@
 <template>
   <main>
-    <div class="login-container" >
+    <div class="login-container">
       <div class="login-module" v-show="isLogin">
         <p>登录</p>
         <div class="login-item">
-          <input type="text" value="" name="account" placeholder="请输入账号" class="account login-input">
-          <input type="password" name="password" placeholder="请输入密码" class="password login-input">
+          <input type="text" v-model="account" placeholder="请输入账号" class="account login-input">
+          <input type="password" v-model="password" placeholder="请输入密码" class="password login-input">
           <div class="image-captcha">
-            <input type="text" placeholder="验证码" class="captcha-input">
-            <img src="../assets/logo.png" alt="验证码">
+            <input type="text" placeholder="验证码" class="captcha-input" v-model="captcha">
+            <img :src="image" alt="验证码" @click="getImageCaptcha" style="cursor: pointer;">
           </div>
-          <input type="button" value="登录" class="login-btn">
+          <input type="button" value="登录" class="login-btn" @click="login">
           <div class="login-other">
             <a class="change-password" href="">忘记密码？</a>
             <a class="register-link" @click="changeStatus">没有账号，去注册！</a>
           </div>
+          <a class="homePage" @click="toHomePage">首页</a>
         </div>
       </div>
       <div class="register-module" v-show="!isLogin">
         <p>注册</p>
         <div class="login-item">
-          <input type="text" value="" name="account" placeholder="请输入手机号码" class="account login-input">
-          <input type="password" name="password" placeholder="请输入密码" class="password login-input">
-          <input type="password" name="confirmPassword" placeholder="请输入确认密码"
+          <input type="text" placeholder="请输入手机号码" class="account login-input" v-model="account1">
+          <input type="password" placeholder="密码，由数字和字母组成且长度8-16" class="password login-input"
+                 v-model="password1">
+          <input type="password" placeholder="请输入确认密码" v-model="password2"
                  class="confirm-password login-input">
           <div class="captcha-module">
-            <input type="text" name="captcha" placeholder="验证码" class="captcha">
-            <input type="button" value="验证码" class="captcha-btn">
+            <input type="text" v-model="captcha1" placeholder="验证码" class="captcha">
+            <input type="button" :disabled="!isClick" v-model="captcha_btn" class="captcha-btn" @click="getSmsCaptcha">
           </div>
-          <input type="button" value="注册" class="login-btn">
+          <input type="button" value="注册" class="login-btn" @click="register">
 
           <div class="login-other">
             <a class="register-link" @click="changeStatus">已有账号，去登录！</a>
           </div>
+          <a class="homePage" @click="toHomePage">首页</a>
         </div>
       </div>
     </div>
@@ -44,13 +47,90 @@ export default {
   name: "LoginRegister",
   data() {
     return {
-      isLogin: true
+      isLogin: true,
+      image: '',
+      text: '',
+      //登录模块，分开实现避免出现bug
+      account: '',
+      password: '',
+      captcha: '',
+      //注册模块
+      account1: '',
+      password1: '',
+      password2: '',
+      captcha1: '',
+      //验证码按钮计时
+      captcha_btn: '验证码',
+      time: 61,
+      isClick:true
     }
+  },
+  mounted() {
+    this.getImageCaptcha();
   },
   methods: {
     changeStatus() {
-      this.isLogin = !this.isLogin
-      console.log(this.isLogin)
+      this.isLogin = !this.isLogin;
+    },
+    toHomePage() {
+      this.$router.push("/");
+    },
+    getImageCaptcha() {
+      this.$http.get("/captcha/image").then(res => {
+        this.image = res.data.data.image;
+        this.text = res.data.data.text;
+      })
+    },
+    getSmsCaptcha() {
+      if (this.account1 === '' || this.account1.length !== 11) {
+        this.$message.error("请输入正确的手机号码！");
+      } else {
+        if (this.time > 0) {
+          this.time--;
+          this.captcha_btn = this.time + "s";
+          this.isClick=false;
+          setTimeout(this.getSmsCaptcha, 1000);
+        } else {
+          this.time = 61;
+          this.captcha_btn = '验证码';
+          this.isClick=true;
+        }
+      }
+    },
+    login() {
+      if (this.text !== this.captcha) {
+        this.$message.error("验证码错误，请注意大小写！")
+      } else if (this.account === '' || this.password === '') {
+        this.$message.error("请账号或密码！")
+      } else {
+        this.$http.post("####################################后端接口").then(res => {
+          if (res.data.code === 200) {
+            this.$message.success("登录成功");
+            //将token存到local storage
+            window.localStorage.setItem("user_token", res.data.data);
+            //自动切换到主页
+            setTimeout(function () {
+              this.$router.push("/");
+            }, 1500)
+          } else {
+            this.$message.error(res.data.data);
+          }
+        })
+      }
+    },
+    register() {
+      var re = /^[0-9a-zA-Z]*$/;
+      if (this.account1 === '' || this.account1.length !== 11) {
+        this.$message.error("请输入正确的手机号码！");
+      } else if (!re.test(this.password1) || this.password1.length < 8 || this.password1.length > 16) {
+        this.$message.error("密码不符合规范！");
+      } else if (this.password1.length === 0 || this.password2.length === 0) {
+        this.$message.error("请输入密码或确认密码！");
+      } else if (this.password1 !== this.password2) {
+        this.$message.error("密码与确认密码不一致！");
+      } else if (this.captcha1.length !== 4) {
+        this.$message.error("验证码长度错误！");
+      }
     }
   }
 }
@@ -60,13 +140,16 @@ export default {
 main {
   width: 100vw;
   height: 100vh;
+  min-height: 650px;
   background: url("../assets/icon/login-bg.png") repeat;
   background-size: cover;
   min-width: 1170px;
 }
-input{
+
+input {
   border-radius: 5px;
 }
+
 .login-module, .register-module {
   float: right;
   margin-top: 12%;
@@ -104,7 +187,8 @@ input{
   font-family: "Microsoft YaHei UI", serif;
   font-size: 16px;
 }
-.captcha-input{
+
+.captcha-input {
   width: 150px;
   height: 25px;
   margin-top: 15px;
@@ -116,7 +200,8 @@ input{
   font-family: "Microsoft YaHei UI", serif;
   font-size: 16px;
 }
-.image-captcha img{
+
+.image-captcha img {
   vertical-align: middle;
   width: 120px;
   height: 40px;
@@ -189,7 +274,19 @@ input{
   cursor: pointer;
 }
 
-.login-other a:hover{
+.login-other a:hover {
   cursor: pointer;
+}
+
+.homePage {
+  display: block;
+  width: 50px;
+  color: #007bff;
+  margin: 0 auto;
+}
+
+.homePage:hover {
+  cursor: pointer;
+  color: #58A7FA;
 }
 </style>
